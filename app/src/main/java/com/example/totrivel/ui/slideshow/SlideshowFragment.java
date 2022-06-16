62182559dc0e1a15e83d03264a28237131d031fb
DESKTOP-F2RQ5U8\Ching <a22024125@gmail.com>
2022-06-16 19:43:33 +0800

    更新所有頁面連結按鈕以及路線UI

--- app/src/main/java/com/example/totrivel/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/totrivel/ui/slideshow/SlideshowFragment.java
@@ -6,11 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 
 import com.example.totrivel.MainActivity;
 import com.example.totrivel.R;
@@ -19,6 +19,7 @@ import com.example.totrivel.databinding.FragmentSlideshowBinding;
 public class SlideshowFragment extends Fragment {
 
     private FragmentSlideshowBinding binding;
+    private Button mrouteCheckBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +31,11 @@ public class SlideshowFragment extends Fragment {
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        mrouteCheckBtn = root.findViewById(R.id.routeCheckBtn);
+        mrouteCheckBtn.setOnClickListener(routeCheckBtnOnClick);
+
+
         return root;
     }
 
@@ -38,4 +44,12 @@ public class SlideshowFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    private View.OnClickListener routeCheckBtnOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view){
+            NavController navController = ((MainActivity) getActivity()).getNavController();
+            navController.navigate(R.id.action_nav_slideshow_to_nav_firstday);
+        }
+    };
 }
